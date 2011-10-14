72e7ba9397269156006a5aa2939aec4e7bf1b7a2
Satish Srinivasan <sathyasvasan@gmail.com>
2011-10-14 13:29:54 +0530

    TODO for 0.1: In-Place Edit, Error Handling, Testing

---
+++ nbugtrack/templates/js/app.js
@@ -0,0 +1,5 @@
+
+/* $(document).ready(function(){
+    $("#proj_listing #proj_data").filter(":odd").css("background-color","#eef");
+});
+*/
--- nbugtrack/view.py
+++ nbugtrack/view.py
@@ -42,16 +42,16 @@ def showView(data):
     
 def show_projects(content):
     ''' list_projects is the controller function '''
-    proj_html = '''<div id="proj_data">
-                   <table class="list_projects">'''
     
+    proj_html = '''<div id="proj_listing"><ol>'''
     for proj_data in content:
         name = str(proj_data[0])
         desc = str(proj_data[1]) # unicode to string
         
-        proj_html += '''<tr><th><a href="'''+name+'''/">'''+name+'''</a></th><td>'''+desc+'''</td></tr>'''
-
-    proj_html += '''</table></div>'''
+        proj_html += '''<li><div id="proj_data">'''
+        proj_html += '''<div class="proj_name"><a href="'''+name+'''/">'''+name+'''</a></div>''' #+'''<div class="proj_desc">'''+desc+'''</div>'''
+        proj_html += '''</div></li>'''
+    proj_html += '''</ol></div>'''
     return default_template.replace("$page_body",proj_html).replace("$page_title","Projects")
 
 def view_this_project(content):
@@ -61,13 +61,9 @@ def view_this_project(content):
     bugs_list = content[2]
     wiki_list = content[3]
     
-    details_html = '''<div id="project_details_data">
-                      <div id="project_name">
-                      ''' + name + '''
-                      <div id="project_description">
-                      ''' + description + '''</div>'''
-    details_html += show_bugs(bugs_list[0])
-    details_html += show_wiki(wiki_list[0])
+    details_html = '''<div id="project_details_data">''' + '''<div id="project_description">''' + description + '''</div></div>'''
+    details_html += "<h2>Bugs</h2>"+show_bugs(bugs_list[0])
+    details_html += "<h2>Wiki</h2>"+show_wiki(wiki_list[0])
 
     return default_template.replace("$page_body",details_html).replace("$page_title","Project: "+name)
 
@@ -75,7 +71,7 @@ def show_bugs(content):
     ''' return a bugs table '''
     bugs_html = '''<div id="bugs_data">
                    <table class="list_bugs">'''
-    bugs_html += '''<tr><th>Id</th><th>Short Name</th><th>Description</th><th>Priority</th><th>Status</th></tr>'''
+    bugs_html += '''<tr><th>Bug Id</th><th>Short Name</th><th>Priority</th><th>Status</th></tr>'''
     print(content)
     
     for bugs_data in content:
@@ -102,22 +98,22 @@ def show_wiki(content):
 def view_this_bug(content):
     bugid, name, desc, prio, stat = [str(i) for i in content[0]]
 
-    bug_html = '''<div id="bug_data"><div id="bug_title">'''+bugid+''': '''+name+'''</div>'''
-    bug_html += '''<div id="bug_stat">'''+stat+'''</div>'''
-    bug_html += '''<div id="bug_prio">'''+prio+'''</div>'''
-
+    bug_html = '''<div id="bug_data">'''
+    bug_html += '''<div id="bug_stat">Status: '''+stat+'''</div>'''
+    bug_html += '''<div id="bug_prio">Priority: '''+prio+'''</div>'''
+    
     if markdown_available == True:
-        bug_html += '''<div id="bug_desc">'''+str(markdown.markdown(desc))+'''</div>'''
+        bug_html += '''<div id="bug_desc">Description: <br />'''+str(markdown.markdown(desc))+'''</div>'''
     else:
-        bug_html += '''<div id="bug_desc">'''+str(desc)+'''</div>'''
-    bug_html += '''</div>'''
+        bug_html += '''<div id="bug_desc">Description <br />'''+str(desc)+'''</div>'''
 
-    return default_template.replace("$page_body",bug_html).replace("$page_title","Bug: "+bugid)
+    bug_html += "</div>"
+    return default_template.replace("$page_body",bug_html).replace("$page_title","Bug: "+name+" ("+bugid+")")
 
 def view_this_wiki(content):
     pageid,pagename,pagecontent = [str(i) for i in content[0]]
 
-    page_html = '''<div id="wiki_page"><div id="wiki_title">'''+pageid+''': '''+pagename+'''</div>'''
+    page_html = '''<div id="wiki_page">'''
 
     if markdown_available == True:
         page_html += '''<div id="wiki_content">'''+str(markdown.markdown(pagecontent))+'''</div>'''
